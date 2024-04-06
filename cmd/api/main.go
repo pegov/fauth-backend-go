92ebac7204d975909c17ccbb59c1fbbc65362d9d
pegov <dvpegov@gmail.com>
2024-04-06 20:59:44 +0300

    impl `render.JSON` and `render.BindJSON`

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,13 +5,24 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/pegov/fauth-backend-go/internal/http/render"
 )
+
+type Foo struct {
+	A string `json:"a"`
+}
 
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+		var foo Foo
+		err := render.BindJSON(r, &foo)
+		if err != nil {
+			return
+		}
+
+		render.JSON(w, http.StatusOK, map[string]any{"message": "ok"})
 	})
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe("localhost:3000", r)
 }
