b84927fd0bf7bf6e1960259b43f3e2777fccd05d
pegov <dvpegov@gmail.com>
2024-05-08 22:22:41 +0300

    add `RealIP` middleware

--- internal/api/router.go
+++ internal/api/router.go
@@ -19,6 +19,7 @@ import (
 func NewRouter() chi.Router {
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(5 * time.Second))
