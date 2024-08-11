e3680dda0656965d17a4953915af5f43a8885188
pegov <dvpegov@gmail.com>
2024-08-11 20:57:35 +0300

    add simple sql init

---
+++ resources/sql/init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS auth_user(
+	id SERIAL PRIMARY KEY,
+	username TEXT NOT NULL,
+	email TEXT NOT NULL,
+	password TEXT,
+	active BOOLEAN DEFAULT TRUE,
+	verified BOOLEAN DEFAULT FALSE,
+	date_added TIMESTAMP WITH TIME ZONE,
+	date_added TIMESTAMP WITH TIME ZONE
+);
+CREATE INDEX IF NOT EXISTS auth_user_username_idx ON auth_user(username);
+CREATE INDEX IF NOT EXISTS auth_user_email_idx ON auth_user(email);
+
+CREATE TABLE IF NOT EXISTS auth_oauth(
+	user_id INTEGER PRIMARY KEY REFERENCES auth_user(id) ON DELETE CASCADE,
+	provider TEXT NOT NULL,
+	sid TEXT NOT NULL
+);
+CREATE INDEX IF NOT EXISTS auth_oauth_provider_sid_idx ON auth_oauth(provider, sid);
