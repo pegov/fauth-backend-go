8ac0743b3e256be8dc74a6f5c1b657363884ed87
pegov <dvpegov@gmail.com>
2024-08-27 20:46:23 +0300

    handle bind json error

--- internal/api/wrapper.go
+++ internal/api/wrapper.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/pegov/fauth-backend-go/internal/api/handler"
+	"github.com/pegov/fauth-backend-go/internal/http/bind"
 	"github.com/pegov/fauth-backend-go/internal/http/render"
 	"github.com/pegov/fauth-backend-go/internal/log"
 	"github.com/pegov/fauth-backend-go/internal/service"
@@ -15,6 +16,7 @@ type HandlerFuncWithError = func(w http.ResponseWriter, r *http.Request) error
 func makeHandler(fn HandlerFuncWithError, logger log.Logger) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var validationError *service.ValidationError
+		var bindJSONError *bind.BindJSONError
 		if err := fn(w, r); err != nil {
 			switch {
 			case errors.Is(err, handler.ErrInvalidPathParamType):
@@ -36,6 +38,10 @@ func makeHandler(fn HandlerFuncWithError, logger log.Logger) http.HandlerFunc {
 				errors.Is(err, service.ErrPasswordVerification),
 				errors.Is(err, handler.ErrNoTokenCookie):
 				render.String(w, http.StatusUnauthorized, "Unauthorized")
+
+			case errors.As(err, &bindJSONError):
+				// TODO: details
+				render.String(w, http.StatusUnprocessableEntity, "Unprocessable entity")
 
 			default:
 				logger.Errorf("Internal server error: %s", err)
