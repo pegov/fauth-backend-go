ed33669a7f28f4293a7f41345ae505b0a1812a13
pegov <dvpegov@gmail.com>
2024-08-29 20:36:14 +0300

    impl std email client

---
+++ internal/email/email.go
@@ -0,0 +1,36 @@
+package email
+
+import (
+	"fmt"
+	"net/smtp"
+)
+
+type EmailClient interface {
+	SendEmail(from, to, message string) error
+}
+
+type StdEmailClient struct {
+	username string
+	password string
+	host     string
+	port     string
+}
+
+func NewStdEmailClient(username, password, host, port string) EmailClient {
+	return &StdEmailClient{
+		username: username,
+		password: password,
+		host:     host,
+		port:     port,
+	}
+}
+
+func (c *StdEmailClient) SendEmail(from, to, message string) error {
+	auth := smtp.PlainAuth("", c.username, c.password, c.host)
+	addr := fmt.Sprintf("%s:%s", c.host, c.port)
+	if err := smtp.SendMail(addr, auth, from, []string{to}, []byte(message)); err != nil {
+		return err
+	}
+
+	return nil
+}
